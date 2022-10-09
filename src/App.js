481edeeca2df17321d416efb6b1a63e3e6531dd8
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-09 07:42:26 +0100

    saved carousel

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Carousel from './Components/Carousel';
+
+export const App = () => {
+  return (
+    <div>
+       <Carousel/>
+    </div>
+  )
+}
---
+++ src/Components/ImageHolder.js
@@ -0,0 +1,33 @@
+const IMAGES = [
+    {
+        id: carousel1,
+        imgSrc: "/images/ai.jpg",
+        imgText: 'Good Begining',
+    },
+    {
+        id: carousel2,
+        imgSrc: "/images/img1.jpg",
+        imgText: 'Imitate Greatness',
+    },
+    {
+        id: carousel3,
+        imgSrc: "/images/img2.jpg",
+        imgText: 'Be Good',
+    },
+    {
+        id: carousel4,
+        imgSrc: "/images/img3.jpg",
+        imgText: 'Think Big',
+    },
+    {
+        id: carousel5,
+        imgSrc: "/images/img4.webp",
+        imgText: 'Stand Strong',
+    },
+    {
+        id: carousel6,
+        imgSrc: "/images/img5.webp",
+        imgText: 'Good Days',
+    },
+]
+export default IMAGES;
