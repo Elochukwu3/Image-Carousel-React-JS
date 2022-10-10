7b71d3aa1fc8c22b321a88bfb6dba303911af055
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-10 02:30:43 +0100

    saved index

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,14 +3,16 @@ import data from './ImageHolder';
 import './App.css';
 export default function Carousel() {
     const [slider, setSlider] = useState(0); 
+
   return (
     <div className='container'>
        <div className='containerInner'>
        {
-            data.length && data.map(m=>{
-                const {id, imgSrc, imgText} = m
+            data.length && data.map((m, index)=> {
+                const {id, imgSrc, imgText} = m;
                 return (
-                <div className='slider'><img src={imgSrc} alt={imgText} /> </div>
+                <div className= {slider === index ? 'slider.active' : 'slider'} 
+                key={id}><img src={imgSrc} alt={imgText} /> </div>
                 )
             })
         }
