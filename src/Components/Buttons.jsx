0210cc8f32391cdb97c653134a1ef7e080f84f37
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-10 05:45:50 +0100

    saved

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,12 +1,12 @@
-
-import React from "react";
-
-    const ARL = 'fa-solid fa-arrow-left';
-    const ARR = 'fa-solid fa-arrow-right';
-export default function Butttons({arrowSide, moveSlide}) { 
-    return (
-    <button onClick={moveSlide} className={arrowSide === 'next' ? 'nextSlide' : 'prevSlide'} >
-        <i className={arrowSide === 'next' ? ARR : ARL}></i>
+const ARL = "fa-solid fa-arrow-left";
+const ARR = "fa-solid fa-arrow-right";
+export default function Butttons({ arrowSide, moveSlide }) {
+  return (
+    <button
+      onClick={moveSlide}
+      className={arrowSide === "next" ? "nextSlide" : "prevSlide"}
+    >
+      <i className={arrowSide === "next" ? ARR : ARL}></i>
     </button>
-  )
+  );
 }
--- src/Components/ImageHolder.js
+++ src/Components/ImageHolder.js
@@ -1,49 +1,48 @@
 const IMAGES = [
-    {
-        id: 'carousel1',
-        imgSrc: "/images/ai.jpg",
-        imgText: 'Good Begining',
-    },
-    {
-        id: 'carousel2',
-        imgSrc: "/images/img1.jpg",
-        imgText: 'Imitate Greatness',
-    },
-    {
-        id: 'carousel3',
-        imgSrc: "/images/img2.jpeg",
-        imgText: 'Be Good',
-    },
-    {
-        id:' carousel4',
-        imgSrc: "/images/img3.jpeg",
-        imgText: 'Think Big',
-    },
-    {
-        id: 'carousel5',
-        imgSrc: "/images/img4.webp",
-        imgText: 'Stand Strong',
-    },
-    {
-        id: 'carousel6',
-        imgSrc: "/images/img9.webp",
-        imgText: 'Good Times are rare',
-    },
-    {
-        id: 'carousel7',
-        imgSrc: "/images/img8.webp",
-        imgText: 'Pray Always',
-    },
-    {
-        id: 'carousel8',
-        imgSrc: "/images/img7.webp",
-        imgText: 'Father help us',
-    },
-    {
-        id: 'carousel9',
-        imgSrc: "/images/img6.jpeg",
-        imgText: 'Good Night',
-    },
-    
-]
+  {
+    id: "carousel1",
+    imgSrc: "/images/ai.jpg",
+    imgText: "Good Begining",
+  },
+  {
+    id: "carousel2",
+    imgSrc: "/images/img1.jpg",
+    imgText: "Imitate Greatness",
+  },
+  {
+    id: "carousel3",
+    imgSrc: "/images/img2.jpeg",
+    imgText: "Be Good",
+  },
+  {
+    id: " carousel4",
+    imgSrc: "/images/img3.jpeg",
+    imgText: "Think Big",
+  },
+  {
+    id: "carousel5",
+    imgSrc: "/images/img4.webp",
+    imgText: "Stand Strong",
+  },
+  {
+    id: "carousel6",
+    imgSrc: "/images/img9.webp",
+    imgText: "Good Times are rare",
+  },
+  {
+    id: "carousel7",
+    imgSrc: "/images/img8.webp",
+    imgText: "Pray Always",
+  },
+  {
+    id: "carousel8",
+    imgSrc: "/images/img7.webp",
+    imgText: "Father help us",
+  },
+  {
+    id: "carousel9",
+    imgSrc: "/images/img6.jpeg",
+    imgText: "Good Night",
+  },
+];
 export default IMAGES;
