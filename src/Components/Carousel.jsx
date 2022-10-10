735e6c33484dd376fab0eeb7be864fdbb2b7f158
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-10 02:16:37 +0100

    saved style

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,18 +1,20 @@
 import {useState} from 'react';
 import data from './ImageHolder';
-
+import './App.css';
 export default function Carousel() {
     const [slider, setSlider] = useState(0); 
   return (
-    <div>
-        {
+    <div className='container'>
+       <div className='containerInner'>
+       {
             data.length && data.map(m=>{
                 const {id, imgSrc, imgText} = m
                 return (
-                <div><img src={imgSrc} alt={imgText} /> </div>
+                <div className='slider'><img src={imgSrc} alt={imgText} /> </div>
                 )
             })
         }
+       </div>
     </div>
   )
 }
